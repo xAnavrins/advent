42a91482b32d77600560c7693d7b944a7fb26e1f
xAnavrins <xanavrins@gmail.com>
2021-12-04 01:36:25 -0500

    Day 4

--- 2021/day3/part1/code.lua
+++ 2021/day3/part1/code.lua
@@ -5,15 +5,16 @@ local total = 0
 
 for line in input do
 	if not bitLen then bitLen = #line end
-	for i = 1, bitLen do
-		gamma[i] = (gamma[i] or 0) + line:sub(i,i)
-	end
+	line:gsub("()(.)", function(pos, num)
+		gamma[pos] = (gamma[pos] or 0) + num
+	end)
 	total = total + 1
 end
 
 for i = 1, bitLen do
-	gamma[i] = (math.floor((gamma[i]/total) + 0.5))
-	epsilon[i] = gamma[i] == 1 and 0 or 1
+	local moreOnes = gamma[i] > (total - gamma[i])
+	gamma[i] = moreOnes and 1 or 0
+	epsilon[i] = moreOnes and 0 or 1
 end
 
 return tonumber(table.concat(gamma), 2) * tonumber(table.concat(epsilon), 2)
---
+++ 2021/day4/part1/code.lua
@@ -0,0 +1,60 @@
+local boards = {}
+local b = 0
+
+for line in input do
+	if #line == 0 then
+		b = b + 1
+	else
+		if b == 0 then
+			boards[b] = {}
+			line:gsub("(%d+)", function(num)
+				table.insert(boards[b], tonumber(num))
+			end)
+		else
+			if not boards[b] then boards[b] = {} end
+			local row = #boards[b]+1
+			boards[b][row] = {}
+			line:gsub("(%d+)", function(num)
+				table.insert(boards[b][row], {tonumber(num), false})
+			end)
+		end
+	end
+end
+
+local winner
+local winDraw
+(function()
+	for _, draw in ipairs(boards[0]) do
+		print("Draw", draw)
+		for nB, board in ipairs(boards) do
+			for nR, row in ipairs(board) do
+				for nC, column in ipairs(row) do
+					if not column[2] then
+						column[2] = column[1] == draw
+					end
+				end
+			end
+		end
+
+		for nB, board in ipairs(boards) do
+			for i = 1, 5 do
+				if (board[i][1][2] and board[i][2][2] and board[i][3][2] and board[i][4][2] and board[i][5][2])
+				or (board[1][i][2] and board[2][i][2] and board[3][i][2] and board[4][i][2] and board[5][i][2]) then
+					print("Winner, board", nB, "draw", draw)
+					winner = board
+					winDraw = draw
+					return
+				end
+			end
+		end
+	end
+end)("fuck me dude")
+
+local sum = 0
+for _, row in ipairs(winner) do
+	for _, column in ipairs(row) do
+		sum = sum + (column[2] == true and 0 or column[1])
+	end
+end
+
+return sum * winDraw
