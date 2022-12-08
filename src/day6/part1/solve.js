851622f0fb10b6518201462690ae0f49207a8d52
xAnavrins <xAnavrins@gmail.com>
2022-12-07 23:40:44 -0500

    Day 6

---
+++ src/day6/part1/solve.js
@@ -0,0 +1,17 @@
+export async function run(input, lines) {
+    let matchSize = 4
+    function hasRepeats(s) {
+        let r = Array.from(s)
+        let b = 0
+        for (let i = 0; i < s.length; i++) {
+            let t = r.pop()
+            b |= r.indexOf(t) + 1
+        }
+        return b !== 0
+    }
+    let i
+    for (i = matchSize; i < input.length; i++)
+        if (!hasRepeats(input.substring(i - matchSize, i))) break
+
+    return i
+}
