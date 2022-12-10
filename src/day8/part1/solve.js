53e89a4a6ef226dc50e4bcfe1fe3ae6e1486a24e
xAnavrins <xAnavrins@gmail.com>
2022-12-09 23:57:04 -0500

    Day 8

---
+++ src/day8/part1/solve.js
@@ -0,0 +1,37 @@
+export async function run(input, lines) {
+    let forest = []
+
+    let i = 0
+    lines.forEach(line => {
+        if (!forest[i]) forest[i] = []
+        forest[i] = line.split("").map(Number)
+        i++
+    })
+
+    function checkLine(x, y, dx, dy, forest, visible) {
+        visible.add(`${x}-${y}`)
+        let max = forest[y][x]
+
+        while (true) {
+            x += dx
+            y += dy
+            if (y < 0 || y >= forest.length || x < 0 || x >= forest[y].length) break
+            if (forest[y][x] > max) {
+                max = forest[y][x]
+                visible.add(`${x}-${y}`)
+            }
+        }
+    }
+
+    let visible = new Set()
+    for (let x = 0; x < forest[0].length; x++) {
+        checkLine(x, 0, 0, 1, forest, visible)
+        checkLine(x, forest.length - 1, 0, -1, forest, visible)
+    }
+    for (let y = 0; y < forest.length; y++) {
+        checkLine(0, y, 1, 0, forest, visible)
+        checkLine(forest[0].length - 1, y, -1, 0, forest, visible)
+    }
+
+    return visible.size
+}
---
+++ src/day8/part2/solve.js
@@ -0,0 +1,38 @@
+export async function run(input, lines) {
+    let forest = []
+
+    let i = 0
+    lines.forEach(line => {
+        if (!forest[i]) forest[i] = []
+        forest[i] = line.split("").map(Number)
+        i++
+    })
+
+    function checkLine(x, y, dx, dy, forest) {
+        let visible = 0
+        let max = forest[y][x]
+
+        while (true) {
+            x += dx
+            y += dy
+            if (y < 0 || y >= forest.length || x < 0 || x >= forest[y].length) break
+            visible++
+            if (forest[y][x] >= max) break
+        }
+
+        return visible
+    }
+
+    let max = 0
+    for (let y = 0; y < forest.length; y++) {
+        for (let x = 0; x < forest[y].length; x++) {
+            let score  = checkLine(x, y, 0, -1, forest)
+                score *= checkLine(x, y, 0,  1, forest)
+                score *= checkLine(x, y, 1,  0, forest)
+                score *= checkLine(x, y,-1,  0, forest)
+            if (score > max) max = score
+        }
+    }
+
+    return max
+}
